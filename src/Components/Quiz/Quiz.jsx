6997d96a4737380e0c29b073482ac2faaabdd0fe
Maureen E <maureenekeugo1@gmail.com>
2024-06-26 01:59:09 -0400

    added functionality for score percentage

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -56,17 +56,26 @@ const Quiz = () => {
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
+        {result?<>
+            <h2>You Scored {score} out of {data.length}</h2>  
+    <button>Reset</button>
+        </>:<>
+        
         <h2>{index+1}. {question.question}</h2>
 
-        <ul>
-            <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-            <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-            <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-            <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
-        </ul>
-        <button onClick={next}>Next</button>
+<ul>
+    <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+    <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+    <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+    <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+</ul>
+<button onClick={next}>Next</button>
 
-        <div className="index">{index+1} of {data.length} questions</div>
+<div className="index">{index+1} of {data.length} questions</div>
+        
+        </>}
+    {/* <h2>You Scored {score} out of {data.length}</h2>  
+    <button>Reset</button> */}
     </div>
   )
 }
